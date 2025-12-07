04f5522c9a4f953e355ac744e4e1a7c6a4abed92
Willzy14 <samwills19@googlemail.com>
2025-12-07 22:12:42 +0000

    Clean up overworld: remove paddock, center cottage, reposition animals

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yarnvale Service Worker - enables offline play and PWA installation
-const CACHE_NAME = 'yarnvale-v3';
+const CACHE_NAME = 'yarnvale-v4';
 const urlsToCache = [
   './',
   './index.html',
