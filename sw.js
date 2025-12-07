006e89452076609a86958b3d07614a84d13075f7
Willzy14 <samwills19@googlemail.com>
2025-12-07 22:57:22 +0000

    Add peacock collision detection

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yarnvale Service Worker - enables offline play and PWA installation
-const CACHE_NAME = 'yarnvale-v8';
+const CACHE_NAME = 'yarnvale-v9';
 const urlsToCache = [
   './',
   './index.html',
