a9bda20aa658ce9bbfacd1b1145ec736a3ff3cf7
Willzy14 <samwills19@googlemail.com>
2025-12-08 13:53:30 +0000

    Fix dye shop touch controls, add gift button for NPCs, add second sheep, bump cache v10

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yarnvale Service Worker - enables offline play and PWA installation
-const CACHE_NAME = 'yarnvale-v9';
+const CACHE_NAME = 'yarnvale-v10';
 const urlsToCache = [
   './',
   './index.html',
