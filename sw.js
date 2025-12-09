b8e5e9da68aaed67edc78dc34eeb53aff846f92e
Willzy14 <samwills19@googlemail.com>
2025-12-09 17:00:04 +0000

    V39: Bump service worker cache (v13) after All-tab layout tweak

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Yarnvale Service Worker - enables offline play and PWA installation
-const CACHE_NAME = 'yarnvale-v12';
+const CACHE_NAME = 'yarnvale-v13';
 const urlsToCache = [
   './',
   './index.html',
